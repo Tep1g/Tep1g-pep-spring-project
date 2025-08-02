1e582b688fe68643b9a294dd75dea41ce063c935
Luis Maria <gamerluis.m@gmail.com>
2025-08-02 04:49:14 +0000

    add methods to service classes

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,8 +1,12 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Account;
 import com.example.repository.AccountRepository;
 
 @Service
@@ -10,4 +14,21 @@ public class AccountService {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    public Optional<Account> register(Account account) {
+        Account newAccount = null;
+        String username = account.getUsername();
+        if (!(username.isBlank()) && (username.length() >= 4)) {
+            try {
+                newAccount = accountRepository.save(account);
+            }
+            // Handle non-unique username
+            catch (ConstraintViolationException exception) {}
+        }
+        return Optional.ofNullable(newAccount);
+    }
+
+    public Optional<Account> login(Account account) {
+        return Optional.ofNullable(accountRepository.getAccountByUserAndPass(account.getUsername(), account.getPassword()));
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,8 +1,14 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Message;
 import com.example.repository.MessageRepository;
 
 @Service
@@ -10,4 +16,43 @@ public class MessageService {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    public Optional<Message> createMessage(Message message) {
+        Message newMessage = null;
+        String messageText = message.getMessageText();
+        if (!(messageText.isBlank() && (messageText.length() < 255))) {
+            try {
+                newMessage = messageRepository.save(message);
+            }
+            // Handle non-existent posted_by user
+            catch (ConstraintViolationException exception) {}
+        }
+        return Optional.ofNullable(newMessage);
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Optional<Message> getMessageById(int id) {
+        return messageRepository.findById(id);
+    }
+
+    public boolean deleteMessageById(int id) {
+        try {
+            messageRepository.deleteById(id);
+            return true;
+        }
+        catch (EmptyResultDataAccessException exception) {
+            return false;
+        }
+    }
+
+    public int updateMessageById(Message message) {
+        return messageRepository.updateMessageById(message.getMessageText(), message.getMessageId());
+    }
+
+    public List<Message> getAllMessagesByAccountId(int accountId) {
+        return messageRepository.getMessagesByAccountId(accountId);
+    }
 }
