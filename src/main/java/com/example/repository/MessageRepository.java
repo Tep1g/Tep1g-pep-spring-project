5e22dcdf69490c68d997f13f0401f1c6fe02c1d3
Luis Maria <gamerluis.m@gmail.com>
2025-08-02 04:48:57 +0000

    add custom message table methods

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,10 +1,20 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Modifying
+    @Query("UPDATE message SET message.messageText = :newMessageText WHERE message.messageId = :id")
+    int updateMessageById(@Param("newMessageText") String newMessageText, @Param("id") Integer id);
+
+    List<Message> getMessagesByAccountId(Integer postedBy);
 }
