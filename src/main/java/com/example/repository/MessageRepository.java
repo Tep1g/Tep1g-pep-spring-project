8f12282cbd3aeb432b21b5c2715b885af7e171c6
Luis Maria <gamerluis.m@gmail.com>
2025-08-02 20:02:34 +0000

    add transactional annotations to update and delete messageRepository methods

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,10 +15,12 @@ import com.example.entity.Message;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
     @Modifying
+    @Transactional
     @Query(value = "UPDATE message m SET m.messageText=:newMessageText WHERE m.messageId=:id", nativeQuery = true)
     int updateMessageByMessageId(@Param("newMessageText") String newMessageText, @Param("id") Integer id);
 
-    int deleteByMessageId(@Param("id") Integer id);
+    @Transactional
+    int deleteByMessageId(Integer messageId);
 
     List<Message> getMessagesByPostedBy(Integer postedBy);
 }
